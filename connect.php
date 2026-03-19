0a351e53c2b9b3378cc7980199eb15c88a3b400f
vaunban <bulaongjohnreico@gmail.com>
2026-03-19 21:11:28 +0800

    echo success

--- connect.php
+++ connect.php
@@ -1,12 +1,15 @@
 <?php
-    $servername = "localhost ";
-    $username = "root";
-    $password = "";
-    $dbname = "posddatabase";
+    $servername = "db45126.public.databaseasp.net";
+    $username = "db45126";
+    $password = "admin123";
+    $dbname = "db45126";
 
     $conn = new mysqli($servername,$username,$password,$dbname);
 
     if($conn->connect_error){
         die("Error!".$conn->connect_error);
+    }
+    else{
+        echo"Connected Successfully";
     }
 ?>
