531120328bab60162994646965d45b68505ad397
Christopher <christopherreforsado225@gmail.com>
2026-04-03 00:20:18 +0800

    you know it, edited the sidebar

--- admin/pages/transactions.php
+++ admin/pages/transactions.php
@@ -18,7 +18,7 @@ if($_SESSION['role'] != 'admin'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/customerstyle.css">
+    <link rel="stylesheet" href="css/transactionstyle.css">
 </head>
 <body>
     
