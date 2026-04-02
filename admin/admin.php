6b2a75570df08a9c78d649c6afba76edf722d2b4
Christopher <christopherreforsado225@gmail.com>
2026-04-03 00:18:50 +0800

    added an s, needed renaming to work

--- admin/admin.php
+++ admin/admin.php
@@ -17,7 +17,7 @@ if($_SESSION['role'] != 'admin'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="pages/css/adminstyle.css">
+    <link rel="stylesheet" href="pages/css/adminsstyle.css">
 </head>
 <body>
         <div class="sidebar">
